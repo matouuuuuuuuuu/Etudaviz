2f1bd2d1ea0afaecef76ca4b49d68256e825efee
Mathis <albrunmathis@gmail.com>
2025-12-12 18:20:37 +0100

    changement mineure

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -564,32 +564,6 @@ function getDebouchesDepuisOnisep(string $intitule, ?string $code = null): ?arra
 
 
 
-function getFranceTravailAccessToken() {
-    $clientId = "PAR_malbrunalwaysdatanet_de07e6739e2412366eaa75b683e3ebf844107c6173c733fd44b9d0822420edef";
-    $clientSecret = "d9d31cae50ebca6fa275c3aaa543b9bbfbc74a910cd123e27f03bf6e6e78b13a";
-
-    $url = "https://entreprise.francetravail.fr/connexion/oauth2/access_token?realm=/partenaire";
-
-    $postFields = http_build_query([
-        "grant_type" => "client_credentials",
-        "client_id" => $clientId,
-        "client_secret" => $clientSecret,
-        "scope" => "api_rome-metiersv1"
-    ]);
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    return json_decode($response, true);
-}
-
-
-
 function ensureSession(): void {
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
